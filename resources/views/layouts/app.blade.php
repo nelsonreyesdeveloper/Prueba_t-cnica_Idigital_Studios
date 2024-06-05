f7c6683de450896e61430e60ef5c071c8915f288
reyes1010 <92321108+reyes1010@users.noreply.github.com>
2024-06-05 13:33:26 -0600

    change name of paginate index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'PRUEBA TECNICA | NELSON REYES') }}</title>
         <script src="https://cdn.tailwindcss.com"></script>
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
